d99736b3af66013b07666b44ea6f990fa195b25e
liyuan <1835630110@qq.com>
2018-08-10 12:31:36 +0800

    8.10

---
+++ src/_118_Pascal_s_Triangle.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * Given a non-negative integer numRows,
+ * generate the first numRows of Pascal's triangle.
+ * In Pascal's triangle,
+ * each number is the sum of the two numbers directly above it.
+ *
+ *
+ *Input: 5
+ * Output:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ *
+ *
+ */
+
+public class _118_Pascal_s_Triangle {
+}
---
+++ src/_35_Search_Insert_Position.java
@@ -0,0 +1,12 @@
+/***
+ * Given a sorted array and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ *You may assume no duplicates in the array.
+ *Input: [1,3,5,6], 5
+ *Output: 2
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ */
+
+public class _35_Search_Insert_Position {
+}
